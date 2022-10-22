b7b32619227f9c887ee4c5b8cfe6beb99276f966
kiendao1412 <105455391+kiendao1412@users.noreply.github.com>
2022-10-23 02:56:55 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://sanphamloi-89689-default-rtdb.firebaseio.com/",
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBRCYPU5OQCFn8pgl5e1B_Kmn5Kw1NMzaw",
+    authDomain: "sanphamloi-89689.firebaseapp.com",
+    databaseURL: "https://sanphamloi-89689-default-rtdb.firebaseio.com",
+    projectId: "sanphamloi-89689",
+    storageBucket: "sanphamloi-89689.appspot.com",
+    messagingSenderId: "75607111391",
+    appId: "1:75607111391:web:29c05d043384f89ec4db6a",
+    measurementId: "G-49D6765K3E"
+  };
 
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
